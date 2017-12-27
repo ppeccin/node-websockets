a90c19f96ffa75602a2754cf064c328c6b6f9651
ppeccin <ppeccin@gmail.com>
2017-12-27 15:12:04 -0200

    WS Test

---
+++ ProxyDownloader.js
@@ -0,0 +1,51 @@
+/**
+ * Created by peccin on 18/07/2016.
+ */
+
+module.exports = {
+    processGet: processGet
+};
+
+var request = require('request');
+
+function processGet(req, res) {
+
+    var url = req.query.url;
+    var origin = (req.headers["Origin"] || req.headers["origin"]);
+
+    console.log(">>> Serving proxy download request: " + url + " from: " + origin);
+
+    // Restrict use?
+    var cors = process.env.CORS_FROM;
+
+    // Error if not from allowed origins
+    if (cors && (!origin || cors.indexOf(origin) < 0)) {
+        console.log(">>> Not allowed!");
+        res.sendStatus(401);
+        return;
+    }
+
+    // Return a good filename
+    res.attachment(url);
+
+    // Fire
+    addCorsResponseHeader(res);
+    console.log(">>> Accepted");
+    request
+        .get(url)
+        .on('response', function(response) {
+            console.log(">>> OK! Response status: " + response.statusCode);
+        })
+        .on('error', function(err) {
+            console.log(">>> Error: " + err);
+            res.sendStatus(400);
+        })
+        .pipe(res);
+
+    function addCorsResponseHeader(response) {
+        if (cors && origin) {
+            response.setHeader("access-control-allow-origin", origin);
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+    }
+}

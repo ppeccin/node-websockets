2d8fecf24771d39d3f44f8b327fd337f7eadc6fa
ppeccin <ppeccin@gmail.com>
2017-12-27 15:23:09 -0200

    WS Test

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const INDEX = path.join(__dirname, 'index.html');
 const PROXY = require('./ProxyDownloader').processGet;
 
 const server = express()
-  .use("/", (req, res) => res.sendFile(INDEX) )
   .use("/proxy-remote-download", PROXY)
+  .use("/", (req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
